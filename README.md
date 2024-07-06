36f0638952e6810a54be6613b4814c6d3a5a58db
Logan Dhillon <logandhi64@gmail.com>
2024-07-06 18:19:56 -0400

    Initial publication

---
+++ README.md
@@ -0,0 +1,3 @@
+# Analog Unicast Data Protocol (AUDP)
+
+AUDP transfers analog data in a unicast manner between two TCP/IP-compliant machines.
---
+++ audp.py
@@ -0,0 +1,57 @@
+"""
+Analog Unicast Data Protocol (AUDP)
+Copyright (c) 2024 Logan Dhillon
+"""
+
+from numpy import float64
+from typing import List, Literal
+
+BIT_DURATION = 0.08
+
+BIT_HIGH = 1600
+BIT_LOW = 800
+BIT_CHUNK_END = 200
+
+TOLERANCE = 50
+
+SAMPLE_RATE = 44100
+WAVE_LENGTH = 3528
+
+bit_to_hz = lambda bit: BIT_HIGH if bit == 1 else BIT_LOW
+
+def hz_to_bit(hz: float64) -> Literal[0, 1, 2, -1]:
+    if (BIT_LOW - TOLERANCE) <= hz <= (BIT_LOW + TOLERANCE):
+        return 0
+    elif (BIT_HIGH - TOLERANCE) <= hz <= (BIT_HIGH + TOLERANCE):
+        return 1
+    elif (BIT_CHUNK_END - TOLERANCE) <= hz <= (BIT_CHUNK_END + TOLERANCE):
+        return 2
+    else:
+        return -1
+    
+
+def analog_to_bytes(frequencies: List[float]) -> List[List[int]]:
+    bits = []
+    bytes_array = []
+
+    for freq in frequencies:
+        bit = hz_to_bit(freq)
+        if bit == 2:
+            if bits:
+                bytes_array.append(bits)
+                bits = []
+        elif bit == -1:
+            continue
+        else:
+            bits.append(bit)
+    
+    if bits:
+        bytes_array.append(bits)
+    
+    return bytes_array
+
+
+class SampleRateMismatch(Exception):
+    def __init__(self, sample_rate):
+        self.message = f"Sample rate mismatch! Expected {SAMPLE_RATE}, got {sample_rate}"
+        super().__init__(self.message)
